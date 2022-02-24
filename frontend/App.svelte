4c60c5e09b7031a4f2c04ea94b247465d940d47e
Marcio Diaz <marcio.diaz@gmail.com>
2022-02-24 15:44:47 +0100

    Update app

--- frontend/App.svelte
+++ frontend/App.svelte
@@ -1,13 +1,41 @@
 <script lang="ts">
+  import { onMount } from "svelte"
   import { auth, createActor } from "./store/auth"
   import Intro from "./Intro.svelte"
   import Auth from "./Auth.svelte"
+  // import MarketItem from "./MarketItem.svelte"
+
+  let item
+  let page
+
+  async function hashchange() {
+    const path = window.location.hash.slice(1)
+
+    if (path.startsWith("/market")) {
+      const id = path.slice(6)
+      // get market data
+      window.scrollTo(0, 0)
+    } else if (path.startsWith("/top")) {
+      page = +path.slice(5)
+      item = null
+    } else {
+      window.location.hash = "/top/1"
+    }
+  }
 </script>
 
-<div class="App">
-  <Auth {auth} {createActor} />
-  <Intro {auth} />
-</div>
+<svelte:window on:hashchange={hashchange} />
+
+<main>
+  {#if item}
+    <div>Hello item</div>
+  {:else}
+    <div class="App">
+      <Auth {auth} {createActor} />
+      <Intro {auth} />
+    </div>
+  {/if}
+</main>
 
 <style global>
   body {
