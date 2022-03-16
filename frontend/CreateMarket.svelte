c21473828f7d2d66ce974a22e46fedd450df19bb
Marcio Diaz <marcio.diaz@gmail.com>
2022-03-16 17:03:06 +0000

    Add market created

--- frontend/CreateMarket.svelte
+++ frontend/CreateMarket.svelte
@@ -3,6 +3,7 @@
 
   export let auth
 
+  let marketCreated = false
   let newMarketTitle
   let newMarketDesc
   let yesDeposit
@@ -26,44 +27,51 @@
       imageUrl: imageUrl,
     }
     await $auth.actor.createMarket(marketInitData)
+    marketCreated = true
   }
 </script>
 
 <Header />
 
-<div class="CreateBody">
-  <div class="FormItem">
-    <div style="">
-      <h4 style="">Create New Market</h4>
-    </div>
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">Title:</div>
-      <div><input bind:value={newMarketTitle} size="80" maxlength="200" /></div>
-    </div>
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">Description:</div>
-      <div><textarea bind:value={newMarketDesc} rows="20" cols="70" /></div>
-    </div>
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">Image URL:</div>
-      <div><input bind:value={imageUrl} size="80" maxlength="200" /></div>
-    </div>
+{#if marketCreated}
+  <div class="CreateBody">Market created!</div>
+{:else}
+  <div class="CreateBody">
+    <div class="FormItem">
+      <div style="">
+        <h4 style="">Create New Market</h4>
+      </div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">Title:</div>
+        <div>
+          <input bind:value={newMarketTitle} size="80" maxlength="200" />
+        </div>
+      </div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">Description:</div>
+        <div><textarea bind:value={newMarketDesc} rows="20" cols="70" /></div>
+      </div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">Image URL:</div>
+        <div><input bind:value={imageUrl} size="80" maxlength="200" /></div>
+      </div>
 
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">Yes deposit:</div>
-      <div><input bind:value={yesDeposit} size="20" maxlength="7" /></div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">Yes deposit:</div>
+        <div><input bind:value={yesDeposit} size="20" maxlength="7" /></div>
+      </div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">No deposit:</div>
+        <div><input bind:value={noDeposit} size="20" maxlength="7" /></div>
+      </div>
+      <div style="padding: 1em; text-align:left; font-size: 0.7em">
+        <div style="font-size: 1.5em">End date:</div>
+        <div><input bind:value={endDate} size="20" maxlength="10" /></div>
+      </div>
+      <button class="demo-button" on:click={createMarket}>Create Market</button>
     </div>
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">No deposit:</div>
-      <div><input bind:value={noDeposit} size="20" maxlength="7" /></div>
-    </div>
-    <div style="padding: 1em; text-align:left; font-size: 0.7em">
-      <div style="font-size: 1.5em">End date:</div>
-      <div><input bind:value={endDate} size="20" maxlength="10" /></div>
-    </div>
-    <button class="demo-button" on:click={createMarket}>Create Market</button>
   </div>
-</div>
+{/if}
 
 <style global>
   .CreateBody {
