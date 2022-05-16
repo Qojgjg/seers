b9b1d385d8fa95e9759802964cac5ee1609bb62d
Marcio Diaz <marcio.diaz@gmail.com>
2022-05-16 12:52:23 +0100

    Put controls on the right side

--- frontend/Forecast.svelte
+++ frontend/Forecast.svelte
@@ -35,72 +35,77 @@
   }
 </script>
 
-<div class="market-controls">
-  <div
-    style="display:flex; justify-content:center; text-align:center; align-items:center;flex-direction:column;width: 200px; padding: 10px;"
-  >
-    <h4>Forecast</h4>
+<div
+  style="margin: 0em 1em;display:flex; justify-content:start; flex-direction:column"
+>
+  <div class="market-controls">
     <div
-      style="width: 200px; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items:center"
+      style="display:flex; justify-content:center; text-align:center; align-items:center;flex-direction:column;width: 100px;"
     >
-      {#if market}
-        {#each market?.labels as label}
-          <div
-            style="display:flex; flex-direction:row; width: 200px; justify-content:center; align-items:center; padding: 5px"
-          >
-            <div style="width: 50px; margin-right: 20px">{label.label}</div>
-            <div style="width: 50px;">
-              <input
-                bind:value={label.value}
-                style="width: 100%; color: rgb(255, 255, 255); background-color: rgb(33, 36, 41); height: 1em; font-family: 'Roboto Mono', monospace; border: 0"
-                placeholder="0.0"
-              /> %
+      <h4>Forecast</h4>
+      <div
+        style="width: 100px; text-align: center; display: flex; flex-direction: column; justify-content: center; align-items:center"
+      >
+        {#if market}
+          {#each market?.labels as label}
+            <div
+              style="display:flex; flex-direction:row; width: 100px; justify-content:center; align-items:center; padding: 5px"
+            >
+              <div style="width: 50px; margin-right: 20px">{label.label}</div>
+              <div style="width: 50px;">
+                <input
+                  bind:value={label.value}
+                  style="width: 100%; color: rgb(255, 255, 255); background-color: rgb(33, 36, 41); height: 1em; font-family: 'Roboto Mono', monospace; border: 0"
+                  placeholder="0.0"
+                /> %
+              </div>
             </div>
+          {/each}
+          <div
+            style="width: 100px; justify-content: center; text-align: center; display: flex; align-items:center"
+          >
+            {#if processing}
+              <button
+                class="btn-grad"
+                on:click={() => 0}
+                style="width: 150px; overflow:hidden"
+              >
+                <img
+                  src={inf}
+                  alt="inf"
+                  style="width: 150%; height: 400%; margin: -75%;"
+                />
+              </button>
+            {:else if principal != ""}
+              <button
+                class="btn-grad"
+                on:click={async () => {
+                  await submitForecast()
+                }}
+              >
+                Submit
+              </button>
+            {:else}
+              <button class="btn-grad" on:click={() => signIn()}>
+                Login
+              </button>
+            {/if}
           </div>
-        {/each}
-        <div
-          style="width: 150px; justify-content: center; text-align: center; display: flex; align-items:center"
-        >
-          {#if processing}
-            <button
-              class="btn-grad"
-              on:click={() => 0}
-              style="width: 150px; overflow:hidden"
-            >
-              <img
-                src={inf}
-                alt="inf"
-                style="width: 150%; height: 400%; margin: -75%;"
-              />
-            </button>
-          {:else if principal != ""}
-            <button
-              class="btn-grad"
-              style="width: 150px"
-              on:click={async () => {
-                await submitForecast()
-              }}
-            >
-              Submit
-            </button>
-          {:else}
-            <button class="btn-grad" on:click={() => signIn()}> Login </button>
+          {#if errorResponse}
+            <div style="width: 150px;text-align:center;color:red">
+              {errorResponse}
+            </div>
+          {:else if done}
+            <div style="width: 150px;text-align:center;color:green">Done!</div>
           {/if}
-        </div>
-        {#if errorResponse}
-          <div style="width: 100%;text-align:center;color:red">
-            {errorResponse}
+        {:else}
+          <div
+            style="width: 100px; justify-content: center; text-align: center; display: flex"
+          >
+            <button class="btn-grad" on:click={() => 0}> Login </button>
           </div>
-        {:else if done}
-          <div style="width: 100%;text-align:center;color:green">Done!</div>
         {/if}
-      {:else}
-        <div
-          style="width: 200px; justify-content: center; text-align: center; display: flex"
-        >
-          <button class="btn-grad" on:click={() => 0}> Login </button>
-        </div>
-      {/if}
+      </div>
     </div>
   </div>
 </div>
--- frontend/ViewMarket.svelte
+++ frontend/ViewMarket.svelte
@@ -139,30 +139,32 @@
   style="display: flex; padding: 30px; align-items: center; justify-content: center; flex-direction:column; text-align: center"
 >
   <div
-    style="background: rgba(0, 0, 0, 0.3); width: 80%; max-width: 1000px;padding: 80px; border-style: none; border-width: 2px; border-radius: 10px"
+    style="display:flex;background: rgba(0, 0, 0, 0.3); width: 80%; max-width: 1000px;padding: 80px; border-style: none; border-width: 2px; border-radius: 10px"
   >
-    <div
-      style="display:flex; flex-direction: row; justify-content:start; text-align:center"
-    >
-      <img
-        style="width: 150px; border-radius: 5px; margin-right: 15px"
-        src={market?.imageUrl}
-        alt="market"
-      />
-      <div style="">
-        <h3>{market?.title}</h3>
+    <div style="width: 70%">
+      <div
+        style="display:flex; flex-direction: row; justify-content:start; text-align:center"
+      >
+        <img
+          style="width: 150px; border-radius: 5px; margin-right: 15px"
+          src={market?.imageUrl}
+          alt="market"
+        />
+        <div style="">
+          <h3>{market?.title}</h3>
+        </div>
+      </div>
+      <div style="width: 600px">
+        <canvas id="myChart" />
+      </div>
+
+      <div style="line-height: 2; text-align:left; font-size: 1.1em">
+        <h3>Market Rules</h3>
+        <SvelteMarkdown source={market?.description} />
       </div>
     </div>
 
-    <div style="width: 600px">
-      <canvas id="myChart" />
-    </div>
-
-    <div style="line-height: 2; text-align:left; font-size: 1.1em">
-      <h3>Market Rules</h3>
-      <SvelteMarkdown source={market?.description} />
-    </div>
-    <div style="width: 100%; display:flex">
+    <div style="width: 30%; display:flex; flex-direction:column">
       <div>
         <Trade
           {auth}
@@ -177,186 +179,11 @@
         <Forecast {auth} {readMarket} {market} {principal} {signIn} />
       </div>
     </div>
-    <div style="width: 100%; display:flex">
+    <!-- <div style="width: 100%; display:flex">
       <Comments {auth} {marketId} {readMarket} {comments} {signIn} />
-    </div>
+    </div> -->
   </div>
 </div>
-{#if market}
-  <!-- 
-  <div class="rowView">
-    <div class="market-comments">
-      <div class="market">
-        {#if market.imageUrl}{/if}
-      </div>
-      <div class="market">
-        {#each market.comments as comment}
-          <div style="margin: 5px 0px">
-            <div style="color:pink; padding: 5px 0px">{comment.author}</div>
-            <SvelteMarkdown source={comment.content} />
-          </div>
-        {/each}
-        <div class="comment-container">
-          <textarea
-            bind:value={comment}
-            rows="10"
-            style="height: auto; width: 100%"
-            placeholder="Please share your insight"
-          />
-          <button class="btn-grad" on:click={postComment}>
-            {commentLabel}
-          </button>
-          <div style="width: 100%;text-align:center;color:red">
-            {commentErrorResponse}
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="v-container">
-      <div class="market-controls">
-        <ul
-          style="list-style-type: none; margin: 0; padding: 10px; width: 100%; line-height: 1.5em"
-        >
-          <h4 style="text-align:center; padding: 3px; margin: 3px">Details</h4>
-
-          <li>
-            <div style="margin:0;padding:0;text-transform:capitalize">
-              Status: {Object.keys(market.state).toString()}
-            </div>
-          </li>
-          <li>
-            Starts: {new Date(
-              parseInt(market.startDate) / 1_000_000,
-            ).toDateString()}
-          </li>
-          <li>
-            Ends: {new Date(
-              parseInt(market.endDate) / 1_000_000,
-            ).toDateString()}
-          </li>
-          <li>Volume: {Number(market.volume).toFixed(2)} &Sigma;</li>
-          <li>
-            Liquidity: {Number(market.liquidity).toFixed(2)} &Sigma;
-          </li>
-        </ul>
-      </div> -->
-  <!-- <div class="market-controls">
-        <div
-          style="display:flex; justify-content:start; text-align:center; align-items:center;flex-direction:column;width: 100%; padding: 10px;"
-        >
-          <h4>Trade</h4>
-          <div class="YesNoOptions">
-            <button
-              class={buyOptClass}
-              on:click={() => {
-                buyOptClass = "BuyOptSelected"
-                sellOptClass = "SellOpt"
-                buyTokens = true
-                buttonLabel = "Buy " + selectedLabel
-              }}>Buy</button
-            >
-            <button
-              class={sellOptClass}
-              on:click={() => {
-                buyOptClass = "BuyOpt"
-                sellOptClass = "SellOptSelected"
-                buyTokens = false
-                buttonLabel = "Sell " + selectedLabel
-              }}>Sell</button
-            >
-          </div>
-          <div class="OutcomeTitle">Pick Outcome:</div>
-          <div class="ContentTab">
-            <select
-              bind:value={selected}
-              style="width: 100%"
-              on:change={() => {
-                selectedLabel = market.labels[selected]
-                if (buyTokens) buttonLabel = "Buy " + selectedLabel
-                else buttonLabel = "Sell " + selectedLabel
-              }}
-            >
-              {#each market.labels as label, i}
-                <option value={i}>
-                  {(Number(market.probabilities[i]) / 1000.0).toFixed(2)} &Sigma;
-                  - {label.slice(0, 20)}
-                </option>
-              {/each}
-            </select>
-            <div class="OutcomeTitle">Amount:</div>
-            <div class="OutcomeTitle">
-              <input
-                bind:value={seerAmount}
-                on:keyup={() => debounce(market.id, seerAmount, 500)}
-                style="width:100%; height: 30px"
-              />
-            </div>
-            <div class="ControlData">
-              <div>LP fee 0.00%</div>
-              <div>
-                {#if buyTokens}
-                  Avg. price {tokensEstimate
-                    ? (seerAmount / tokensEstimate).toFixed(2)
-                    : 0} &Sigma;
-                {:else}
-                  Avg. price {tokensEstimate
-                    ? (seerAmount / tokensEstimate).toFixed(2)
-                    : 0} tokens
-                {/if}
-              </div>
-              <div>
-                {#if buyTokens}
-                  Max. Winnings {tokensEstimate
-                    ? Number(tokensEstimate).toFixed(2)
-                    : Number(0).toFixed(2)}
-                  &Sigma;
-                {:else}
-                  Get back {tokensEstimate
-                    ? Number(tokensEstimate).toFixed(2)
-                    : Number(0).toFixed(2)} &Sigma;
-                {/if}
-              </div>
-            </div>
-            <div
-              style="width: 100%; text-align: center; display: flex; flex-direction: column; justify-content: center"
-            >
-              {#if principal !== ""}
-                <Modal show={$modal}>
-                  <Content
-                    onOk={() => {
-                      let res = doIt(market.id, seerAmount)
-                      seerAmount = 0.0
-                      tokensEstimate = 0.0
-                      return res
-                    }}
-                    {tokensEstimate}
-                    {seerAmount}
-                    outcome={selectedLabel}
-                    {buttonLabel}
-                    {buyTokens}
-                  />
-                </Modal>
-                <div style="width: 100%;text-align:center;color:red">
-                  {errorResponse}
-                </div>
-              {:else}
-                <div
-                  style="width: 100%; justify-content: center; text-align: center; display: flex"
-                >
-                  <button class="btn-grad" on:click={() => signIn()}>
-                    Login
-                  </button>
-                </div>
-              {/if}
-            </div>
-          </div>
-        </div>
-      </div> 
-    </div>
-  </div>-->
-{:else}
-  <div class="header">Loading</div>
-{/if}
 
 <style global>
   .comment-container {
@@ -515,10 +342,6 @@
       /* width: 300px;
       min-width: 0px;
       max-width: 80%; */
-    }
-
-    .market-controls {
-      margin: 1em;
     }
 
     .market-comments {
