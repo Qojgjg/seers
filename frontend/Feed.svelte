52cdc9df2843b1537b5e44b40ff984036a73de42
Marcio Diaz <marcio.diaz@gmail.com>
2022-05-21 00:21:51 +0100

    Short time

--- frontend/Feed.svelte
+++ frontend/Feed.svelte
@@ -38,35 +38,35 @@
     if (diff <= 1) {
       return "just now"
     }
-    if (diff < 20) {
+    if (diff < 60) {
       return diff + " seconds ago"
     }
-    if (diff < 40) {
-      return "half a minute ago"
-    }
-    if (diff < 60) {
-      return "less than a minute ago"
-    }
+    // if (diff < 40) {
+    //   return "half a minute ago"
+    // }
+    // if (diff < 60) {
+    //   return "less than a minute ago"
+    // }
     if (diff <= 90) {
-      return "one minute ago"
+      return "1m"
     }
     if (diff <= 3540) {
-      return Math.round(diff / 60) + " minutes ago"
+      return Math.round(diff / 60) + "m"
     }
     if (diff <= 5400) {
-      return "1 hour ago"
+      return "1h"
     }
     if (diff <= 86400) {
-      return Math.round(diff / 3600) + " hours ago"
+      return Math.round(diff / 3600) + "h"
     }
     if (diff <= 129600) {
-      return "1 day ago"
+      return "1d"
     }
     if (diff < 604800) {
-      return Math.round(diff / 86400) + " days ago"
+      return Math.round(diff / 86400) + "d"
     }
     if (diff <= 777600) {
-      return "1 week ago"
+      return "1w"
     }
     return "on " + system_date
   }
